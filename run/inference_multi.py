e3e9d7204126789d5e0440d9f61652e3734ccf7d
siaaib <123885844+siaaib@users.noreply.github.com>
2023-11-26 21:37:53 +0300

    Update inference_multi.py

--- run/inference_multi.py
+++ run/inference_multi.py
@@ -43,6 +43,7 @@ def get_test_dataloader(cfg: DictConfig) -> DataLoader:
     """
     feature_dir = Path("/kaggle/working/processed_data/test")
     series_ids = [x.name for x in feature_dir.glob("*")]
+    print(series_ids)
     chunk_features = load_chunk_features(
         duration=cfg.duration,
         feature_names=cfg.features,
